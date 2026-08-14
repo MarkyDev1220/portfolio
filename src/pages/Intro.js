651a30ab8c755f6bcfdd425bca0b3e4a444fa728
Your Name <you@example.com>
2026-08-14 19:22:35 -0400

    new intro

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -3,25 +3,13 @@ export default function Intro() {
     <section id="intro" className="intro-section">
       <h1 className="intro-title">About Me</h1>
 
-      <p className="intro-text">
-        Hello there, Welcome to my portfolio! 
-        My name is Markevian Jones (Mark for short), I am a Android Developer with a strong background in 
-        SCADA and industrial operations. I specialize in building modern mobile 
-        applications using Kotlin, Jetpack Compose, MVVM, Firebase, and scalable 
-        architecture patterns. My experience in real‑time industrial systems 
-        gives me a unique advantage in automation, reliability, and problem‑solving 
-        under pressure.
-      </p>
-
       <p className="Professional Summary-intro-text">
-        Android Developer specializing in Kotlin-based mobile application development using Android SDK, Jetpack Compose, MVVM, and Clean
-        Architecture. Developed healthcare and real-time multiplayer applications integrating Firebase, WebRTC, CameraX, and StateFlow,
-        reducing Firestore reads by 30% and synchronization latency by 25% through targeted architectural improvements. Applies systematic
-        debugging, root-cause analysis, and system monitoring developed through SCADA-controlled industrial operations to software problem
-        solving and application reliability. I enjoy creating  apps and feature ideas that are fun and professional for user base use
-        and building software that solves real‑world problems. 
-        My goal is to continue growing as a mobile, web, and software developer and engineer 
-        while expanding into cross‑platform development and backend systems.
+        Hello, My Name is Markevian Jones.
+        I’m an Android Developer with a background in SCADA and industrial operations. 
+        I build modern mobile apps using Kotlin, Jetpack Compose, MVVM, and Firebase. 
+        My experience with real‑time industrial systems gives me a strong foundation in automation, r
+        eliability, and problem‑solving. I enjoy creating tools that help people and designing intuitive, 
+        scalable software.
       </p>
     </section>
   );
